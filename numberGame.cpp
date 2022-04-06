c3547690e42fc3c038f396601cbcce9a863bfb43
Ubuntu <ubuntu@ip-172-31-85-29.ec2.internal>
2022-04-06 01:41:02 +0000

    code

--- numberGame.cpp
+++ numberGame.cpp
@@ -1,24 +1,27 @@
 // Copyright (C) 2022 Devin Jhu All rights reserved
 //
 // Created by Devin Jhu
-// Created on March 2022
+// Created on April 2022
 // The number game
 
 #include <iostream>
 #include <random>
+#include <string>
+
 
 
 int main() {
     // this function is a guessing game
+    std::string GUESS_STRING;
+     int GUESS;
     int NUMBER_ANSWER;
 
     std::random_device rseed;
     // mersenne_twister
     std::mt19937 rgen(rseed());
-    // randome number between [1,9]
+    // random number between [1,9]
     std::uniform_int_distribution <int> idist(1 , 9);
     NUMBER_ANSWER = idist(rgen);
-    int GUESS;
 
     std::cout << "The number game";
     std::cout << "" << std::endl;
@@ -26,14 +29,19 @@ int main() {
     // input
     std::cout << "" << std::endl;
     std::cout << "enter number: ";
-    std::cin >> GUESS;
+    std::cin >> GUESS_STRING;
 
     // process
-    if (GUESS == NUMBER_ANSWER) {
-        std::cout << "You win!";
-    } else {
-        std::cout << "You lose :(";
+    try {
+        GUESS = std::stoi(GUESS_STRING);
+        if (GUESS == NUMBER_ANSWER) {
+           std::cout << "You win!";
+        } else {
+            std::cout << "You lose :(";
+        }
+    } catch (std::invalid_argument) {
+        std::cout << "\nNot a number";
     }
     std::cout << "" << std::endl;
-    std::cout << "Done.";
+    std::cout << "\nDone.";
 }
